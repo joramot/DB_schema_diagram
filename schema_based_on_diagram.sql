51729ca2c60ed404eea76b9e97379d4917ac27be
Jose RM <mojica.raul@gmail.com>
2023-11-08 11:13:10 -0600

    Add indexes to the tables with foreign keys, reorganize the schema file, and change the name to one tablee

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,4 +1,4 @@
-CREATE TABLE patiens (
+CREATE TABLE patients (
     id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100),
     date_of_birth DATE,
@@ -11,8 +11,10 @@ CREATE TABLE medical_histories (
     patient_id INT,
     status VARCHAR(50),
     PRIMARY KEY (id),
-    FOREIGN KEY (patient_id) REFERENCES patiens(id)
+    FOREIGN KEY (patient_id) REFERENCES patients(id)
 );
+
+CREATE INDEX idx_medical_histories_patient_id ON medical_histories (patient_id);
 
 CREATE TABLE invoices (
     id INT GENERATED ALWAYS AS IDENTITY,
@@ -24,6 +26,18 @@ CREATE TABLE invoices (
     FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
 );
 
+CREATE INDEX idx_invoices_medical_history_id ON invoices (medical_history_id);
+
+CREATE TABLE treatments (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    type VARCHAR(100),
+    name VARCHAR(100),
+    PRIMARY KEY (id)
+);
+
+CREATE INDEX idx_medical_histories_treatments_medical_histories_id ON medical_histories_treatments (medical_histories_id);
+CREATE INDEX idx_medical_histories_treatments_treatments_id ON medical_histories_treatments (treatments_id);
+
 CREATE TABLE invoice_items (
     id INT GENERATED ALWAYS AS IDENTITY,
     unit_price REAL,
@@ -32,28 +46,9 @@ CREATE TABLE invoice_items (
     invoce_id INT,
     treatment_id INT,
     PRIMARY KEY (id),
+    FOREIGN KEY (invoce_id) REFERENCES invoices(id),
     FOREIGN KEY (treatment_id) REFERENCES treatments(id)
 );
 
-CREATE TABLE treatments (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    type VARCHAR(100),
-    name VARCHAR(100),
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE treatments (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    type VARCHAR(100),
-    name VARCHAR(100),
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE medical_histories_treatments (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    medical_histories_id INT,
-    treatments_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (medical_histories_id) REFERENCES medical_histories(id),
-    FOREIGN KEY (treatments_id) REFERENCES treatments(id)
-);
+CREATE INDEX idx_invoices_invoce_id ON invoice_items (invoce_id);
+CREATE INDEX idx_invoices_treatment_id ON invoice_items (treatment_id);
